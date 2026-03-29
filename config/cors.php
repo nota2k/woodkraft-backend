4b54fdd7db8d9eecc552a3891845d3a58b6b8835
nota2k <nelly.babillon@gmail.com>
2026-03-29 20:13:19 +0200

    chore: add production domain to CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'https://woodkraft.nellybabillon.fr',
+    ],
 
     'allowed_origins_patterns' => [],
 
